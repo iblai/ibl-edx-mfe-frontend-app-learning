219eccdda23befa81f29ad858fb156e81bf06e75
gipsbrian <gipsbrian@gmail.com>
2025-12-03 13:50:33 +0000

    Restore fetch call in TabContainer to fix loading spinner issue

--- src/tab-page/TabContainer.jsx
+++ src/tab-page/TabContainer.jsx
@@ -19,14 +19,12 @@ const TabContainer = (props) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // DEBUGGING: Skip all backend calls - just show static page
-    // TODO: Re-enable when iframe works
-    // if (isProgressTab) {
-    //   dispatch(fetch(courseIdFromUrl, targetUserId));
-    // } else {
-    //   dispatch(fetch(courseIdFromUrl));
-    // }
-    console.log('[JWT Auth] TabContainer: Skipping fetch for debugging - no backend calls');
+    // The courseId from the URL is the course we WANT to load.
+    if (isProgressTab) {
+      dispatch(fetch(courseIdFromUrl, targetUserId));
+    } else {
+      dispatch(fetch(courseIdFromUrl));
+    }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [courseIdFromUrl, targetUserId]);
 
